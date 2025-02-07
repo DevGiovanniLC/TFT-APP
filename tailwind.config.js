f665e29ea9a37c48b41c2a4ee1321623b7e25fc5
Giovanni León <giovannilc.dev@gmail.com>
2025-02-07 18:06:03 +0000

    Eliminado componentes por defecto y prevenido el mal linting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+/* eslint-disable no-undef */
+
+/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
+
   plugins: [require('@tailwindcss/aspect-ratio')
-,require('@tailwindcss/forms')
-,require('@tailwindcss/typography')
-],
+
+    , require('@tailwindcss/forms')
+
+    , require('@tailwindcss/typography')
+  ],
 };

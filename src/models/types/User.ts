643ce3a300cecca43e7a237718ef3a3c2bd26f55
Giovanni León <giovannilc.dev@gmail.com>
2025-03-28 20:56:40 +0000

    MVP: Actualización de tipos de datos y agregado pipe para visualización de fechas

--- src/models/types/User.ts
+++ src/models/types/User.ts
@@ -2,10 +2,19 @@ import { WeightUnits } from "./Weight";
 
 export type User = {
     name: string;
-    email: string;
     age: number;
     height: number;
+    gender: Gender;
+
+    email: string;
+
     goal_weight: number;
     goal_units: WeightUnits;
     goal_date: Date;
+}
+
+export enum Gender {
+    MALE = 'male',
+    FEMALE = 'female',
+    OTHER = 'other',
 }
---
+++ src/pipes/SimpleDate.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe,  type PipeTransform } from '@angular/core';
+import { formatDate } from '@angular/common';
+
+@Pipe({
+    name: 'simpleDate'
+})
+export class SimpleDatePipe implements PipeTransform {
+    transform(value: Date | string | number| undefined, format: string = 'dd/MM/yyyy'): string {
+        if (!value) return '';
+        return formatDate(value, format, 'en-US');
+    }
+}

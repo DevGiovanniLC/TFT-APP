5d779ebaaef74c470756726d838e59070ce4ee45
Giovanni León <giovannilc.dev@gmail.com>
2025-03-31 19:19:50 +0100

    Agregados tabs coming soon

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { IonContent, IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
 @Component({
     selector: 'app-tab2',
     templateUrl: 'tab2.page.html',
     styleUrls: ['tab2.page.scss'],
-    imports: [],
+    imports: [IonContent, IonHeader, IonToolbar, IonTitle],
 })
 export class Tab2Page {
     constructor() {}
--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { IonContent, IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
 
 @Component({
     selector: 'app-tab3',
     templateUrl: 'tab3.page.html',
     styleUrls: ['tab3.page.scss'],
-    imports: [],
+    imports: [IonContent, IonHeader, IonToolbar, IonTitle],
 })
 export class Tab3Page {
     constructor() {}

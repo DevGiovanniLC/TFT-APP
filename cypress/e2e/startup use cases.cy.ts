8c7bf4bdb006a48f091ee9f4dcfd9d3491a5eee6
Giovanni León <giovannilc.dev@gmail.com>
2025-04-07 15:28:06 +0100

    Configuración de cypress agregada

---
+++ cypress/e2e/startup use cases.cy.ts
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+
+describe('Startup use cases', () => {
+    beforeEach(() => {
+        cy.visit('tabs/tab1')
+    })
+
+    it('Without a goal', () => {
+
+    })
+
+})

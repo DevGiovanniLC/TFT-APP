35876e71aa6f70c457678434b9f824dadd5a1fa6
Giovanni León <giovannilc.dev@gmail.com>
2025-04-11 18:08:10 +0100

    Test de comienzo de aplicación finalizados

--- cypress/e2e/startup use cases/goal-flex.cy.js
+++ cypress/e2e/startup use cases/goal-flex.cy.js
@@ -1,4 +1,3 @@
-/// <reference types="cypress" />
 
 let LOCAL_STORAGE_SNAPSHOT = {}
 
@@ -6,7 +5,6 @@ describe('Startup without a goal', () => {
 
     before(() => {
         cy.visit('tabs/tab1')
-
         cy.get('#nextBtn').click()
 
         cy.get('ion-picker-column-option[ng-reflect-value="100"]')
@@ -15,25 +13,22 @@ describe('Startup without a goal', () => {
         cy.get('#nextBtn').click()
         cy.get('#nextBtn').click()
         cy.get('#nextBtn').click()
-
-
     })
 
     beforeEach(() => {
         cy.visit('tabs/tab1')
-
         Object.keys(LOCAL_STORAGE_SNAPSHOT).forEach((key) => {
             localStorage.setItem(key, LOCAL_STORAGE_SNAPSHOT[key]);
         });
     })
 
-    it('Comprobar gráfica principal de peso actual', () => {
+    it('Verify main current weight chart', () => {
         LOCAL_STORAGE_SNAPSHOT = { ...localStorage }
-
+        // cy.captureCanvasImage('#doughnutChart canvas', 'doughnut-chart.png')
         cy.compareCanvasImage('#doughnutChart canvas', 'doughnut-chart.png', 'diff-doughnutChart.png')
     })
 
-    it('Comprobar modos de visualización disponibles', () => {
+    it('Verify available display modes', () => {
         cy.get('#chartMode').click()
         cy.get('div.alert-radio-label.sc-ion-alert-md').contains('View Goal').should('not.exist')
         cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Total').should('be.visible')
@@ -41,7 +36,7 @@ describe('Startup without a goal', () => {
         cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Last Month').should('be.visible')
     })
 
-    it('Comprobar gráfica de registro de peso (Mode Total)', () => {
+    it('Verify weight log chart (Total Mode)', () => {
         cy.get('#chartMode').click()
         cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Total').should('be.visible')
         cy.get('button.alert-button').contains('OK').click()
@@ -49,7 +44,7 @@ describe('Startup without a goal', () => {
         cy.compareCanvasImage('#lineChart canvas', 'line-chart-total.png', 'diff-line-chart-total.png')
     })
 
-    it('Comprobar gráfica de registro de peso (Mode Last Week)', () => {
+    it('Verify weight log chart (Last Week Mode)', () => {
         cy.get('#chartMode').click()
         cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Last Week').should('be.visible').click()
         cy.get('button.alert-button').contains('OK').click()
@@ -58,7 +53,7 @@ describe('Startup without a goal', () => {
         cy.compareCanvasImage('#lineChart canvas', 'line-chart-week.png', 'diff-line-chart-last-week.png')
     })
 
-    it('Comprobar gráfica de registro de peso (Mode Last Month)', () => {
+    it('Verify weight log chart (Last Month Mode)', () => {
         cy.get('#chartMode').click()
         cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Last Month').should('be.visible').click()
         cy.get('button.alert-button').contains('OK').click()
@@ -66,15 +61,9 @@ describe('Startup without a goal', () => {
         cy.compareCanvasImage('#lineChart canvas', 'line-chart-month.png', 'diff-line-chart-last-month.png')
     })
 
-    it ('Comprobar que el peso recomendado al registrar peso es el actual', () => {
+    it('Verify recommended weight for registration matches current weight', () => {
         cy.get('#registerBtn').click()
         cy.get('app-weight-form').should('be.visible').should('have.attr','ng-reflect-input-weight-value', '100')
     })
-
-
-
-
-
-
 
 })
---
+++ cypress/e2e/startup use cases/goal.cy.js
@@ -0,0 +1,79 @@
+/// <reference types="cypress" />
+
+let LOCAL_STORAGE_SNAPSHOT = {}
+
+describe('Startup a goal', () => {
+
+    before(() => {
+        cy.visit('tabs/tab1')
+
+        cy.get('#nextBtn').click()
+
+        cy.get('ion-picker-column-option[ng-reflect-value="120"]')
+            .realClick()
+
+        cy.get('#nextBtn').click()
+        cy.get('#goalBtn').click()
+        cy.wait(1000)
+        cy.get('ion-picker-column-option[ng-reflect-value="80"]')
+        .realClick()
+        cy.wait(1000)
+        cy.get('#confirmBtn').click()
+        cy.get('#nextBtn').click()
+        // cy.pause()
+        cy.get('#nextBtn').click()
+    })
+
+    beforeEach(() => {
+        cy.visit('tabs/tab1')
+
+        Object.keys(LOCAL_STORAGE_SNAPSHOT).forEach((key) => {
+            localStorage.setItem(key, LOCAL_STORAGE_SNAPSHOT[key]);
+        });
+    })
+
+    it('Verify main current weight chart', () => {
+        LOCAL_STORAGE_SNAPSHOT = { ...localStorage }
+        // cy.captureCanvasImage('#doughnutChart canvas', 'goal-doughnut-chart.png')
+        cy.compareCanvasImage('#doughnutChart canvas', 'goal-doughnut-chart.png', 'diff-doughnutChart.png')
+    })
+
+    it('Verify available display modes', () => {
+        cy.get('#chartMode').click()
+        cy.get('div.alert-radio-label.sc-ion-alert-md').contains('View Goal').should('not.exist')
+        cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Total').should('be.visible')
+        cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Last Week').should('be.visible')
+        cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Last Month').should('be.visible')
+    })
+
+    it('Verify weight log chart (Total Mode)', () => {
+        cy.get('#chartMode').click()
+        cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Total').should('be.visible')
+        cy.get('button.alert-button').contains('OK').click()
+        // cy.captureCanvasImage('#lineChart canvas', 'goal-line-chart-total.png')
+        cy.compareCanvasImage('#lineChart canvas', 'goal-line-chart-total.png', 'diff-line-chart-total.png')
+    })
+
+    it('Verify weight log chart (Last Week Mode)', () => {
+        cy.get('#chartMode').click()
+        cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Last Week').should('be.visible').click()
+        cy.get('button.alert-button').contains('OK').click()
+
+        // cy.captureCanvasImage('#lineChart canvas', 'goal-line-chart-week.png')
+        cy.compareCanvasImage('#lineChart canvas', 'goal-line-chart-week.png', 'diff-line-chart-last-week.png')
+    })
+
+    it('Verify weight log chart (Last Month Mode)', () => {
+        cy.get('#chartMode').click()
+        cy.get('div.alert-radio-label.sc-ion-alert-md').contains('Last Month').should('be.visible').click()
+        cy.get('button.alert-button').contains('OK').click()
+        // cy.captureCanvasImage('#lineChart canvas', 'goal-line-chart-month.png')
+        cy.compareCanvasImage('#lineChart canvas', 'goal-line-chart-month.png', 'diff-line-chart-last-month.png')
+    })
+
+    it('Verify recommended weight for registration matches current weight', () => {
+        cy.get('#registerBtn').click()
+        cy.get('app-weight-form').should('be.visible').should('have.attr','ng-reflect-input-weight-value', '120')
+    })
+
+})

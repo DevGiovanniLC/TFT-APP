49b40f5abd22c35ac0091013412ae126a6800def
Giovanni León <giovannilc.dev@gmail.com>
2025-04-15 21:14:17 +0100

    register: Agregado componente individual de registro

---
+++ src/app/pages/tabsPage/registerPage/components/ItemRegister/ItemRegister.component.ts
@@ -0,0 +1,47 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, effect, input, OnInit, signal } from '@angular/core';
+import { Weight } from '@models/types/Weight';
+import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { SimpleDatePipe } from '@pipes/SimpleDate.pipe';
+import { SignedNumberPipe } from '@pipes/signedNumber.pipe';
+
+@Component({
+    selector: 'app-item-register',
+    imports: [
+        IonCard,
+        IonCardHeader,
+        IonCardSubtitle,
+        IonCardTitle,
+        IonCardContent,
+        IonButton,
+        SignedNumberPipe,
+        SimpleDatePipe,
+    ],
+    templateUrl: './ItemRegister.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ItemRegisterComponent implements OnInit {
+    id = input.required<number>();
+    weight = input.required<Weight>();
+    progress = input.required<number>();
+
+
+    color = signal('')
+
+    constructor(private readonly cdr: ChangeDetectorRef) {
+        effect(() => {
+            this.color.set(this.checkColor(this.progress()));
+            this.cdr.detectChanges();
+        })
+    }
+
+    ngOnInit(): void {
+
+    }
+
+    checkColor(progress: number): string {
+        if (progress >= 1.5) return '#be6363';
+        if (progress > 0.5) return '#f7d25c';
+        if (progress < 0 ) return '#b3ea97';
+        return '#e8e8e8'
+    }
+}

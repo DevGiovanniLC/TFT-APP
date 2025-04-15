1e10d4d1e6cf57cd41e5c65f46f43337709bf0e4
Giovanni León <giovannilc.dev@gmail.com>
2025-04-15 21:14:59 +0100

    register: Tab de registros creada

--- src/app/pages/tabsPage/registerPage/register.page.ts
+++ src/app/pages/tabsPage/registerPage/register.page.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { IonContent, IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import { ItemRegisterComponent } from './components/ItemRegister/ItemRegister.component';
+import { Weight, WeightUnits } from '@models/types/Weight';
+import { WeightTrackerService } from '@services/WeightTracker.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 @Component({
     selector: 'app-tab2',
     templateUrl: 'register.page.html',
-    imports: [IonContent, IonHeader, IonToolbar, IonTitle],
+    imports: [IonContent, IonHeader, IonToolbar, IonTitle, ItemRegisterComponent],
 })
 export class RegisterPage {
-    constructor() {}
+    registers = toSignal(this.weightTracker.weights$);
+
+    readonly reversedRegisters = computed(() => {
+        const list = this.registers()?.reverse();
+        return list?.map((curr, i) => {
+            const prev = list[i + 1];
+            const progress = Number((prev ? curr?.weight - prev?.weight : 0).toFixed(2));
+            return { ...curr, progress };
+        });
+    });
+
+
+    constructor(private readonly weightTracker: WeightTrackerService) {
+        this.weightTracker.updateWeights().subscribe();
+    }
+
+
+
+
 }

42b1239e06173ed5727d06ffcd72bf09f91016b9
Giovanni León <giovannilc.dev@gmail.com>
2025-05-10 17:46:14 +0100

    refactor: elimina la implementación de OnInit en HomePage para simplificar el código

--- src/app/pages/tabsPage/home/home.page.ts
+++ src/app/pages/tabsPage/home/home.page.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
 import { WeightTrackerService } from '@services/WeightTracker.service';
 import { Weight } from '@models/types/Weight';

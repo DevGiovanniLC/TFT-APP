88ff580fc3f0935937d0690aa12664ea7e742622
Giovanni León <giovannilc.dev@gmail.com>
2025-05-16 18:15:24 +0100

    feat: agrega funcionalidad de zoom al gráfico de peso y actualiza la lógica de datos

---
+++ src/app/models/charts/ModalWeightChart.ts
@@ -0,0 +1,208 @@
+import { Signal } from '@angular/core';
+import { Goal } from '@models/types/Goal';
+import { Weight } from '@models/types/Weight';
+import { CalculationFunctionsService } from '@services/CalculationFunctions.service';
+import { ChartData, ChartOptions } from 'chart.js';
+import { AnnotationOptions, LineAnnotationOptions } from 'chartjs-plugin-annotation';
+
+export default class ModalWeightChart {
+    private readonly weights: Weight[];
+    private readonly goal: Goal | undefined;
+    private readonly trendData: { x: number; y: number; }[];
+
+    constructor(
+        calculateFunctionsService: CalculationFunctionsService,
+        weights: Weight[],
+        goal: Goal | undefined
+    ) {
+        this.weights = weights;
+        this.goal = goal;
+        this.trendData = calculateFunctionsService.getTrendData(this.weights)
+    }
+
+    getData(): ChartData<'line'> {
+
+        return {
+            labels: this.weights?.map((w: Weight) => new Date(w.date).getTime()),
+            datasets: [
+                {
+                    label: 'Weight (kg)',
+                    data: this.weights?.map((w: Weight) => w.weight),
+                    fill: false,
+                    borderColor: '#00BD7E',
+                    tension: 0.1,
+                    pointRadius: 4,
+                },
+                {
+                    label: 'Trend',
+                    data: this.trendData,
+                    parsing: false,
+                    fill: false,
+                    borderDash: [6, 6],
+                    borderColor: '#ff6384',
+                    pointRadius: 0,
+                    tension: 0,
+                    segment: {
+                        borderDash: [6, 6],
+                    },
+                    backgroundColor: 'transparent',
+                    hoverBackgroundColor: 'transparent',
+                    hoverBorderColor: 'transparent',
+                    pointHoverRadius: 0,
+                    pointHitRadius: 0,
+                },
+            ],
+        };
+    }
+
+    annotationConfig(goalWeight: number, goalDate: Date | undefined) {
+        if (!goalWeight) return {};
+
+        const goalLabel: AnnotationOptions = {
+            type: 'label',
+            yValue: goalWeight + 3,
+            xValue: goalDate?.getTime() ?? NaN,
+            content: ['Goal'],
+            padding: 0,
+            color: '#343A40',
+            font: {
+                size: 11,
+            },
+        };
+
+        const goalLine: LineAnnotationOptions = {
+            yMin: goalWeight,
+            yMax: goalWeight,
+            borderColor: '#343A40',
+            borderWidth: 2,
+            borderDash: [5, 5],
+        };
+
+        if (!goalDate)
+            return {
+                goalLabel,
+                goalLine,
+            };
+
+        const goalPoint: AnnotationOptions = {
+            type: 'point',
+            xScaleID: 'x',
+            yScaleID: 'y',
+            xValue: goalDate?.getTime(),
+            yValue: goalWeight,
+            backgroundColor: '#1E8260',
+            radius: 5,
+            borderWidth: 2,
+            borderColor: '#00BD7E',
+        };
+
+        return {
+            goalPoint,
+            goalLabel,
+            goalLine,
+        };
+    }
+
+    getOptions(): ChartOptions<'line'> {
+        const dataWeights = this.weights;
+
+        // Verifica si el modo de gráfico es 'viewGoal' y si hay suficientes datos para mostrar la tendencia de una manera lógica
+        const goalWeight = this.goal && typeof this.goal === 'object' ? this.goal?.weight : Number.POSITIVE_INFINITY;
+
+        // Rango de pesos para el eje Y
+        const minWeight = Math.min(...dataWeights.map((w) => w.weight));
+        const maxWeight = Math.max(...dataWeights.map((w) => w.weight));
+        const maxRangeY = goalWeight ? maxWeight - Math.min(minWeight, goalWeight) : maxWeight - minWeight;
+        const marginY = maxRangeY * 0.2 || 1;
+
+        // Rango de fechas para el eje X
+        const validDates = dataWeights.map((w) => new Date(w.date));
+        const maxDate = Math.max(...validDates.map((w) => w.getTime()));
+        const minDate = Math.min(...validDates.map((w) => w.getTime())) - 1 * 24 * 60 * 60 * 1000;
+        const rangeX = new Date(maxDate)
+        const goalDateMaxRange = new Date(rangeX.getTime() + 15 * 24 * 60 * 60 * 1000).getTime();
+
+
+
+        return {
+            responsive: true,
+            maintainAspectRatio: false,
+
+            backgroundColor: '#00BD7E',
+            elements: {
+                point: {
+                    hitRadius: 6,
+                    hoverRadius: 7,
+                },
+            },
+            plugins: {
+                annotation: {
+                    annotations: this.annotationConfig(this.goal?.weight ?? NaN, this.goal?.date),
+                },
+                legend: {
+                    display: this.trendData.length > 1,
+                    onClick: () => { },
+                    position: 'top',
+                },
+                zoom: {
+                    zoom: {
+                        wheel: {
+                            enabled: true,
+                        },
+                        pinch: {
+                            enabled: true,
+                        },
+                    },
+                    pan: {
+                        enabled: true,
+                        mode: 'xy',
+                    },
+                }
+            },
+            animation: { duration: 0 },
+            scales: {
+                x: {
+                    min: minDate,
+                    max: goalDateMaxRange,
+                    type: 'time',
+                    time: {
+                        unit: 'day',
+                        displayFormats: {
+                            day: 'MMM dd',
+                        },
+                    },
+                    title: {
+                        display: false,
+                        text: 'Date',
+                    },
+                    ticks: {
+                        padding: 15,
+                        color: '#343a40',
+                        maxTicksLimit: 8,
+                        font: {
+                            size: 15,
+                        }
+                    },
+                },
+                y: {
+                    min: goalWeight
+                        ? Math.round(Math.min(minWeight, goalWeight) - marginY)
+                        : Math.round(minWeight - marginY),
+                    max: goalWeight
+                        ? Math.round(Math.max(maxWeight, goalWeight) + marginY)
+                        : Math.round(maxWeight + marginY),
+                    title: {
+                        display: false,
+                        text: 'Weights (kg)',
+                    },
+                    ticks: {
+                        font: {
+                            size: 16,
+                        },
+                        color: '#343a40',
+                    },
+                },
+            },
+        };
+    }
+}
--- src/app/pages/tabsPage/home/components/WeightGraphicModal/WeightGraphicModal.component.ts
+++ src/app/pages/tabsPage/home/components/WeightGraphicModal/WeightGraphicModal.component.ts
@@ -1,18 +1,55 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, input, Signal } from '@angular/core';
 import { ModalHeaderComponent } from '@components/modals/components/ModalHeader/ModalHeader.component';
 import { ModalController } from '@ionic/angular/standalone';
+import { ChartModule } from 'primeng/chart';
+import { Chart, ChartData, ChartOptions } from 'chart.js';
+import zoomPlugin from 'chartjs-plugin-zoom';
+import { Weight } from '@models/types/Weight';
+import { Goal } from '@models/types/Goal';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CalculationFunctionsService } from '@services/CalculationFunctions.service';
+import { WeightTrackerService } from '@services/WeightTracker.service';
+import { UserConfigService } from '@services/UserConfig.service';
+import ModalWeightChart from '@models/charts/ModalWeightChart';
+
+
 
 @Component({
     selector: 'app-weight-graphic-modal',
-    imports: [ModalHeaderComponent],
+    imports: [ModalHeaderComponent, ChartModule],
     templateUrl: './WeightGraphicModal.component.html',
+    styleUrl: './WeightGraphicModal.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WeightGraphicModalComponent {
+    data!: ChartData<'line'>;
+    options!: ChartOptions<'line'>;
 
-    constructor(private readonly modalCtrl: ModalController) { }
+    private readonly goal = toSignal(this.userService.goal$);
+    private readonly weights = toSignal(this.weightTrackerService.weights$);
 
-    cancel() {
+    constructor(
+        private readonly modalCtrl: ModalController,
+        private readonly calculateFunctionsService: CalculationFunctionsService,
+        private readonly weightTrackerService: WeightTrackerService,
+        private readonly userService: UserConfigService
+    ) {
+        Chart.register(zoomPlugin);
+
+        effect(() => {
+            this.updateWeightChart(this.weights(), this.goal());
+        });
+    }
+
+    updateWeightChart(weights: Weight[] | undefined, goal: Goal | undefined) {
+        if (!weights) return;
+        const weightChart = new ModalWeightChart(this.calculateFunctionsService, weights, goal);
+        this.data = weightChart.getData();
+        this.options = weightChart.getOptions();
+    }
+
+
+    exit() {
         this.modalCtrl.dismiss();
     }
 }

82bc97cdaed30b98707cc6e4dbb860167aebecb5
Giovanni León <giovannilc.dev@gmail.com>
2025-05-17 19:31:24 +0100

    fix: simplifica la lógica y mejora la presentación del servicio de configuración de usuario

--- src/app/services/UserConfig.service.ts
+++ src/app/services/UserConfig.service.ts
@@ -1,45 +1,34 @@
 import { Injectable } from '@angular/core';
 import { DataProviderService } from './data-providers/DataProvider.service';
 import { User } from '@models/types/User';
-import { BehaviorSubject, from, map, Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, from } from 'rxjs';
 import { Goal } from '@models/types/Goal';
+import { map, tap } from 'rxjs/operators';
 
-@Injectable({
-    providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class UserConfigService {
-    private readonly userSubject = new BehaviorSubject<User | undefined>(undefined);
-    private readonly goalSubject = new BehaviorSubject<Goal | undefined>(undefined);
+    private userSubject = new BehaviorSubject<User | undefined>(undefined);
+    private goalSubject = new BehaviorSubject<Goal | undefined>(undefined);
 
     readonly user$ = this.userSubject.asObservable();
     readonly goal$ = this.goalSubject.asObservable();
 
-    constructor(private readonly dataProvider: DataProviderService) {}
+    constructor(private dataProvider: DataProviderService) { }
 
-    getUser() {
+    getUser(): Observable<User | undefined> {
         return from(this.dataProvider.getUser()).pipe(
-            tap((user) => {
-                if (user) {
-                    this.userSubject.next(user);
-                } else this.userSubject.next(undefined);
-            })
+            tap(user => this.userSubject.next(user ?? undefined))
         );
     }
 
     getGoal(): Observable<Goal | undefined> {
         return from(this.dataProvider.getGoal()).pipe(
-            map((goal) => {
-                if (!goal) return undefined;
-                return {
-                    ...goal,
-                    date: goal.date ? new Date(goal.date) : undefined,
-                } as Goal;
-            }),
-            tap((goal) => this.goalSubject.next(goal ?? undefined))
+            map(goal => goal ? { ...goal, date: goal.date ? new Date(goal.date) : undefined } : undefined),
+            tap(goal => this.goalSubject.next(goal))
         );
     }
 
-    setUser(user: User) {
+    setUser(user: User): void {
         this.dataProvider.setUser(user);
         this.getUser().subscribe();
         this.getGoal().subscribe();

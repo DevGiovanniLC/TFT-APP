2fa181752ba439eaa3b9133b2e2e4a5dbf49013a
Giovanni León <giovannilc.dev@gmail.com>
2025-05-20 11:34:27 +0100

    refactor: optimiza la lógica en WeightForm para mejorar el manejo de pesos y simplificar el código

--- src/app/components/forms/WeightForm/WeightForm.component.ts
+++ src/app/components/forms/WeightForm/WeightForm.component.ts
@@ -1,5 +1,4 @@
 import {
-    AfterViewInit,
     ChangeDetectionStrategy,
     ChangeDetectorRef,
     Component,
@@ -7,6 +6,7 @@ import {
     OnInit,
     output,
     signal,
+    AfterViewInit,
 } from '@angular/core';
 import { IonPicker, IonPickerColumn, IonPickerColumnOption, PickerColumn } from '@ionic/angular/standalone';
 import { WeightUnits } from '@models/types/Weight';
@@ -20,49 +20,50 @@ import { WeightUnits } from '@models/types/Weight';
 export class WeightFormComponent implements OnInit, AfterViewInit {
     inputWeightValue = input.required<number | undefined>();
     inputWeightUnit = input(WeightUnits.KG);
-
     outputWeightValue = output<number>();
 
     lastWeight = signal(70);
     lastWeightDecimal = signal(0);
 
-    weightOptions!: number[];
-    weightOptionsDecimal!: number[];
+    weightOptions: number[] = [];
+    weightOptionsDecimal: number[] = [];
 
-    constructor(private readonly cdr: ChangeDetectorRef) {}
+    constructor(private readonly cdr: ChangeDetectorRef) {
+        this.weightOptions.push(...this.generateRange(25, 100));
+    }
 
     ngOnInit(): void {
-        this.weightOptions = this.generateRange(30, 100);
+        this.weightOptions.push(...this.generateRange(101, 176));
         this.weightOptionsDecimal = this.generateRange(0, 9);
-        this.lastWeight.set(Math.floor(this.inputWeightValue() ?? 0));
-        this.lastWeightDecimal.set(Math.round(((this.inputWeightValue() ?? 0) % 1) * 10));
+        const value = this.inputWeightValue() ?? 0;
+        this.lastWeight.set(Math.floor(value));
+        this.lastWeightDecimal.set(Math.round((value % 1) * 10));
     }
 
     ngAfterViewInit(): void {
-        this.weightOptions = this.weightOptions.concat(this.generateRange(101, 250));
+        this.weightOptions.push(...this.generateRange(177, 250));
         this.cdr.detectChanges();
     }
 
-    updateActualWeight(value: PickerColumn['options'][number]['value']) {
-        if (typeof value !== 'number') return;
-
+    updateActualWeight(value: number) {
         this.lastWeight.set(value);
-        this.outputWeightValue.emit(this.lastWeight() + this.lastWeightDecimal() / 10);
+        this.emitWeight();
     }
 
-    updateActualWeightDecimal(value: PickerColumn['options'][number]['value']) {
-        if (typeof value !== 'number') return;
-
+    updateActualWeightDecimal(value: number) {
         this.lastWeightDecimal.set(value);
+        this.emitWeight();
+    }
+
+    columnValueToNumber(value: PickerColumn['options'][number]['value']): number {
+        return Number(value);
+    }
+
+    private emitWeight() {
         this.outputWeightValue.emit(this.lastWeight() + this.lastWeightDecimal() / 10);
     }
 
-    generateRange(start: number, end: number): number[] {
-        const length = end - start + 1;
-        const range = new Array(length);
-        for (let i = 0; i < length; i++) {
-            range[i] = start + i;
-        }
-        return range;
+    private generateRange(start: number, end: number): number[] {
+        return Array.from({ length: end - start + 1 }, (_, i) => start + i);
     }
 }

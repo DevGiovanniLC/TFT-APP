c2110652d6769210c79f3c124d609b40a23649f5
Giovanni León <giovannilc.dev@gmail.com>
2025-05-31 10:51:16 +0100

    language: Añade soporte para traducción con ngx-translate y archivos de idioma en inglés y español

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@capacitor/splash-screen';
 import { UserConfigService } from '@services/UserConfig.service';
 import { WeightTrackerService } from '@services/WeightTracker.service';
 import { EventAdviceService } from '@services/EventAdvice.service';
+import { TranslateService } from '@ngx-translate/core';
+import { DeviceInfoService } from '@services/DeviceInfo.service';
+
 
 @Component({
     selector: 'app-root',
@@ -17,26 +20,39 @@ export class AppComponent {
         private readonly navCtrl: NavController,
         private readonly config: UserConfigService,
         private readonly weightTracker: WeightTrackerService,
+        private readonly deviceInfo: DeviceInfoService,
+        private readonly translate: TranslateService,
         private readonly eventAdvice: EventAdviceService, // No quitar necesario para inicializar constructor
     ) {
         this.initApp();
     }
 
-    initApp() {
+    private initApp() {
         this.dataProvider.initialize().then((connectionStatus) => {
             if (!connectionStatus) throw new Error('Connection to database failed, please try again or reinstall the app');
-            this.weightTracker.getWeights().subscribe();
-            this.config.getGoal().subscribe();
-            this.config.getUser().subscribe((user) => {
-                if (!user) {
-                    this.navCtrl.navigateRoot('/initial');
-                }
-            });
-
-            setTimeout(() => {
-                SplashScreen.hide();
-            }, 500);
-
+            this.initTranslate();
+            this.initData();
+            this.hideSplash();
         });
+    }
+
+    private initTranslate() {
+        this.translate.setDefaultLang(this.deviceInfo.getLanguage());
+    }
+
+    private initData() {
+        this.weightTracker.getWeights().subscribe();
+        this.config.getGoal().subscribe();
+        this.config.getUser().subscribe((user) => {
+            if (!user) {
+                this.navCtrl.navigateRoot('/initial');
+            }
+        });
+    }
+
+    private hideSplash() {
+        setTimeout(() => {
+            SplashScreen.hide();
+        }, 500);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,26 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { defineCustomElements } from 'jeep-sqlite/loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { importProvidersFrom } from '@angular/core';
 
 window.addEventListener('DOMContentLoaded', () => {
     defineCustomElements(window);
 });
 
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 bootstrapApplication(AppComponent, {
     providers: [
+        provideHttpClient(),
         { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
         provideIonicAngular({ mode: 'md' }),
         provideRouter(routes, withPreloading(PreloadAllModules)),
@@ -24,5 +33,14 @@ bootstrapApplication(AppComponent, {
                 preset: Aura,
             },
         }),
+        importProvidersFrom(
+            TranslateModule.forRoot({
+                loader: {
+                    provide: TranslateLoader,
+                    useFactory: HttpLoaderFactory,
+                    deps: [HttpClient]
+                }
+            })
+        )
     ],
 });

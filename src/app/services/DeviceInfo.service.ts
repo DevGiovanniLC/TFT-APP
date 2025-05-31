1e8b9daf16ac316446a843b794f94aa5b5f4ca96
Giovanni León <giovannilc.dev@gmail.com>
2025-05-31 12:05:58 +0100

    refactor: Mejora la gestión del idioma en DeviceInfoService, eliminando código innecesario y asegurando la configuración del idioma por defecto

--- src/app/services/DeviceInfo.service.ts
+++ src/app/services/DeviceInfo.service.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Device, DeviceInfo, GetLanguageCodeResult } from '@capacitor/device';
+import { Device } from '@capacitor/device';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
     providedIn: 'root'
 })
 export class DeviceInfoService {
-    private deviceInfo: DeviceInfo | null;
-    private language: string;
-    constructor() {
-        this.deviceInfo = null;
-        this.language = 'en';
+    private readonly AVAILABLE_LANGUAGES = ['en', 'es'];
+    private readonly DEFAULT_LANGUAGE = 'en';
+
+    private language!: string;
+
+    constructor(
+        private readonly translate: TranslateService
+    ) {
         this.init();
     }
 
     private init() {
         Device.getLanguageCode().then(languageCode => {
-            this.language = languageCode.value;
+            this.language = this.AVAILABLE_LANGUAGES.includes(languageCode.value) ? languageCode.value : this.DEFAULT_LANGUAGE;
+            this.translate.setDefaultLang(this.language);
+        }).catch(() => {
+            const languageCode = this.translate.getBrowserLang() ?? this.DEFAULT_LANGUAGE;
+            this.language = this.AVAILABLE_LANGUAGES.includes(languageCode) ? languageCode : this.DEFAULT_LANGUAGE;
         });
-
-        Device.getInfo().then(info => {
-            this.deviceInfo = info;
-        });
-
     }
 
     getLanguage(): string {

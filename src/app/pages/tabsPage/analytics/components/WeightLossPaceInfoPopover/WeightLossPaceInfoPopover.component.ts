feb40353919dd9030f121fe16485a970a0513549
Giovanni León <giovannilc.dev@gmail.com>
2025-06-01 20:40:36 +0100

    language: Añade soporte para traducción en WeightLossPaceInfoPopover, incluyendo título y mensaje en los archivos de traducción

--- src/app/pages/tabsPage/analytics/components/WeightLossPaceInfoPopover/WeightLossPaceInfoPopover.component.ts
+++ src/app/pages/tabsPage/analytics/components/WeightLossPaceInfoPopover/WeightLossPaceInfoPopover.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-weight-loss-pace-info-popover',
-    imports: [],
+    imports: [TranslateModule],
     templateUrl: './WeightLossPaceInfoPopover.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })

705c99b1d868ba2bebacce8f507430ea61802456
Giovanni León <giovannilc.dev@gmail.com>
2025-06-19 11:49:18 +0100

    fix: mejora la visualización de tendencias de pérdida de peso y agrega formato para números firmados

--- src/app/pages/tabsPage/analytics/components/WeightLossPace/WeightLossPace.component.ts
+++ src/app/pages/tabsPage/analytics/components/WeightLossPace/WeightLossPace.component.ts
@@ -12,10 +12,11 @@ import { Weight, WeightUnits } from '@models/types/Weight.type';
 import { WeightAnalysisService } from '@services/WeightAnalysis.service';
 import { WeightLossPaceInfoPopoverComponent } from '../WeightLossPaceInfoPopover/WeightLossPaceInfoPopover.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { SignedNumberPipe } from '@pipes/signedNumber.pipe';
 
 @Component({
     selector: 'app-weight-loss-pace',
-    imports: [IonButton, IonIcon, IonCard, IonCardTitle, IonCardSubtitle, TranslateModule],
+    imports: [IonButton, IonIcon, IonCard, IonCardTitle, IonCardSubtitle, TranslateModule, SignedNumberPipe],
     templateUrl: './WeightLossPace.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -92,14 +93,39 @@ export class WeightLossPaceComponent implements OnInit {
         this.isTrend.set(false);
     }
 
-    protected validateGoal(){
+
+
+    protected validateGoal() {
         if (this.goal()?.date) return true;
         return false;
     }
 
+    protected checkTrendPace(value: number): boolean {
+        if (isNaN(value) || !value || !this.isTrend()) return false;
+        return true;
+    }
+
     protected validateTrendPace(value: number, range: number): string {
-        if (isNaN(value) || !value || !this.isTrend()) return 'Not Logic';
-        if (value >= -range && value <= range) return `${value.toFixed(2)} ${this.weightUnits()}`;
+        if (!this.checkTrendPace(value)) return 'Not Logic';
+        if (value >= -range && value <= range) return `${value.toFixed(2)}`;
         return 'Not Logic';
     }
+
+    protected checkTextColor(value: number, goal?: number): string {
+        if (isNaN(value) || !value) return 'black';
+        if (goal && value <= goal) return 'var(--color-tertiary)';
+
+        if (value > 0) return '#be6363';
+        if (value < 0) return 'var(--color-secondary)';
+        return 'var(--color-secondary)'
+    }
+
+    protected checkBorderColor(value: number): string {
+        if (isNaN(value) || !value) return 'black';
+
+        if (value > 0) return '#be6363';
+        if (value < 0) return 'var(--color-tertiary)';
+        return 'var(--color-secondary)'
+    }
+
 }
--- src/app/pipes/signedNumber.pipe.ts
+++ src/app/pipes/signedNumber.pipe.ts
@@ -3,12 +3,18 @@ import { Pipe, type PipeTransform } from '@angular/core';
     name: 'signedNumber',
 })
 export class SignedNumberPipe implements PipeTransform {
-    transform(value: number | undefined): string {
+    transform(value: number | undefined | string): string {
         if (!value) return '';
 
-        if (value > 0) return `▲+${value.toFixed(1)}`;
+        if (typeof value === 'string') {
+            const parsedValue = parseFloat(value);
+            if (isNaN(parsedValue)) return value;
+            value = parsedValue;
+        }
 
-        if (value < 0) return `▼${value.toFixed(1)}`;
+        if (value > 0) return `▲ ${value.toFixed(1)}`;
+
+        if (value < 0) return `▼ ${(-1 * value).toFixed(1)}`;
 
         return value.toString();
     }
